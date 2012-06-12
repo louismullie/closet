ac56b01542efd149b560438d6ba88faaabfed54d
Louis Mullie <louis.mullie@gmail.com>
2012-06-12 15:06:21 -0400

    Cosmetic changes.

--- config/omniauth.rb
+++ config/omniauth.rb
@@ -4,3 +4,7 @@ use OmniAuth::Builder do
   provider :facebook, 'key', 'secret'
   provider :twitter, 'key', 'secret'
 end
+
+use OmniAuth::Strategies::Developer
+use OmniAuth::Strategies::Twitter
+use OmniAuth::Strategies::Facebook
