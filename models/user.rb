ace0d70f5fcc756b4bd3b46baae0950cdcebb0f6
Louis Mullie <louis.mullie@gmail.com>
2012-06-12 15:07:24 -0400

    Make our user a bit more realistic.

--- models/user.rb
+++ models/user.rb
@@ -1,4 +1,27 @@
-class App::User
+class User
+  
+  require 'date'
+  
   include Mongoid::Document
+  
+  has_many :questions, validate: false
+  has_many :posts, validate: false
+  has_many :comments, validate: false
+  
+  belongs_to :discussions
+
   field :name, type: String
+  field :email, type: String
+  field :city, type: String
+  field :province, type: String
+  field :country, type: String
+  field :info, type: String
+  
+  field :birth, type: Date
+  field :joined, type: Date
+  
+  field :privileges, type: Array
+  field :flags, type: Array
+
+  
 end
