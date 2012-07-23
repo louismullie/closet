bd9388268382a2b3c92d1d61e12bcdd14efc08c6
Louis Mullie <louis.mullie@gmail.com>
2012-07-23 15:34:44 -0400

    Added I18N.

--- app.rb
+++ app.rb
@@ -1,18 +1,28 @@
-# Require dependencies.
+# Require bundler.
 require 'bundler'
+# Require all gems.
 Bundler.require
 
+# Require all models.
+Dir['./models/*.rb'].each do |m|; require m; end
+
+# Our application namespace.
 module App
   
+  # Require the base class.
   require './base'
-  
+
+  # Our main Sinatra application.
   class Application < Base
     
     configure { require_all 'config', but: 'capistrano'  }
+    before do
+      I18n.locale = params[:locale] || I18n.default_locale
+      @logged_in = !session[:session_id].nil?
+    end
     helpers   { require_all 'helpers' }
     require_all 'routes'
-    require_all 'models'
-    
+  
   end
-
+  
 end
