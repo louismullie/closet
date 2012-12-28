147358bdb73b1e9664433a8f07010666db17154e
Louis Mullie <louis.mullie@gmail.com>
2012-12-28 00:33:58 -0500

    Refactor login route.

--- routes/auth.rb
+++ routes/auth.rb
@@ -1,32 +1,10 @@
-def login(auth)
-  session[:user] = {
-    id: auth['uid'],
-    name: auth.info.name
-  }
-end
-
-post '/auth/twitter/callback' do
-  if login(request.env['omniauth.auth'])
-    redirect '/'
-  else
-    redirect '/login?msg=failed'
-  end
-end
-
-post '/auth/facebook/callback' do
-  login request.env['omniauth.auth']
+get '/auth/:provider/callback' do
+  auth = request.env["omniauth.auth"]
+  session[:user] = User.login(auth)
   redirect '/'
 end
 
-post '/auth/google/callback' do
-  login request.env['omniauth.auth']
+get '/logout' do
+  session[:user] = nil
   redirect '/'
-end
-
-post '/auth/developer/callback' do
-  if login(request.env['omniauth.auth'])
-    redirect '/'
-  else
-    redirect '/login?msg=failed'
-  end
 end
