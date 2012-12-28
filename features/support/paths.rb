86824f0ef952d3ef94b7e603cb8a404c979f32ed
Louis Mullie <louis.mullie@gmail.com>
2012-12-28 03:16:25 -0500

    Stylistic change in automatically generated file.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -9,16 +9,14 @@ module NavigationHelpers
     case page_name
     when /the home\s?page/
       '/'
-
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
     else
       raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
+      "Now, go and add a mapping in #{__FILE__}"
     end
   end
 end
