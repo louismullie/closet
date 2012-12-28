7c2b48dec2b56a036f7824219cb6fb4cbbc4aa3f
Louis Mullie <louis.mullie@gmail.com>
2012-12-28 00:30:54 -0500

    Update to Mongo3 configuration and add mongoid.yml

--- config/mongoid.rb
+++ config/mongoid.rb
@@ -1,6 +1,3 @@
-Mongoid.configure do |config|
-    name = "closet"
-    host = "localhost"
-    port = 27017
-    config.master = Mongo::Connection.new.db(name)
-end
+require 'mongoid'
+
+Mongoid.load!("./config/mongoid.yml")
