fddaf1da03ddeb0df2214c74c5125dca2c61a51a
Louis Mullie <louis.mullie@gmail.com>
2012-12-28 00:30:26 -0500

    Make config file more readable.

--- config/i18n.rb
+++ config/i18n.rb
@@ -1,2 +1,8 @@
-I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'config', 'locales', '*.yml').to_s]
+require 'i18n'
+
+file = File.dirname(__FILE__)
+path = [file, 'config', 'locales', '*.yml']
+config = File.join(*path)
+
+I18n.load_path += Dir[config]
 I18n.default_locale = :en
